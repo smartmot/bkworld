91ae6375e41fa17849fc64f91aada25b7aab910f
EL MOT <mot_edu@outlook.com>
2020-10-03 13:18:31 +0700

    Upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url("public/css/bkworld.css") }}">
+    <title>BK WORLD</title>
+    <style>
+        #show{
+            background-image:url("https://img.wallpapersafari.com/desktop/1920/1080/4/30/FOxrha.jpg");
+            background-size: 100% auto;
+            background-position: center;
+            background-repeat: no-repeat;
+            width:100%;
+            height: 100%;
+        }
+    </style>
+</head>
+<body id="show">
+<!--<div class="p-r h_50 wp_100 pb_10">
+    <div class="wp_100 h_50 bc_blk p-f"></div>
+</div> -->
+<div class="hp_100 wp_100" style="background-color: rgba(255,255,255,0.50)">
+    <div class="bc_whi cwm hp_100">
+        <div class="pt_10 pr_10 pb_10 pl_10">
+            <div class="t_a_c fm-a fs_28 pb_10 pt_5 fw_b">BK WORLD DELEBOPMENT Co,LTD</div>
+            <div>
+                <img src="{{ url("public/icon/bk_world_logo.jpg") }}" class="wp_100">
+            </div>
+            <div class="fs_30 t_a_c fw_b fm-a pb_10 pt_10">Coming soon...</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
