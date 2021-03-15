7b8f9201d42d68f9cda90d49781a150da4550efe
MOT EL <mot_edu@outlook.com>
2021-03-15 16:22:52 +0700

    Update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,10 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title", config("app.name"))</title>
     <link rel="shortcut icon" href="{{ asset("bkworld.svg") }}">
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}" type="text/css">
 </head>
 <body>
-<div class="cw">
-
+<div class="wp_100 h_50 p-r">
+    <div class="wp_100 h_50 p-f t-0 r-0 bc-bk">
+        <div class="cw lh_50">
+            <div class="h_50">
+                <a href="#">
+                    <img class="h_50" src="{{ asset("bkworld.svg") }}" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
