0af5d55e6f779d46e50405f0d991a3bd38d2e4d7
MOT EL <mot_edu@outlook.com>
2021-03-18 14:34:14 +0700

    Update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login(){
         return view("login");
+    }
+
+    public function login_check(Request $request){
+        $validator = Validator::make($request->all(),[
+            "email" => ["required","email","exists:users,email"],
+            "password" => ["required"]
+        ]);
+        $login= $validator->validate();
+        $login["status"] = "activated";
+        $pass = Validator::make($request->only("password"),[
+            "password" => ["required"]
+        ]);
+        if (Auth::attempt($login)) {
+            // Authentication passed...
+            return redirect(route("home"));
+        }else{
+            return $pass->after(function ($pass){
+                $pass->errors()->add("password","Incorrect Password!");
+            })->validate();
+        }
     }
 }
