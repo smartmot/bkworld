c407c8a3223d542696304da816afbd450377e5f9
EL MOT <mot_edu@outlook.com>
2021-04-16 15:06:28 +0700

    Update

--- resources/views/admin/event_edit.blade.php
+++ resources/views/admin/event_edit.blade.php
@@ -3,7 +3,6 @@
     <a class="fm-ubt4 fs_16 pr_10 pl_10 pt_3 pb_4 t_d_n color_1 b_r_3 bcolor_5 hbcolor_4 abcolor_4 box-s1" href="{{ route("event.index") }}">Back</a>
 @endsection
 
-
 @section("content")
     <div class="pt_5">
         <form action="{{ route("event.update", $event->id) }}" method="post" spellcheck="false" autocomplete="off">
