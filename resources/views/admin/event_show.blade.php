4f71107075d8395a59870fed250ff16668ba1360
EL MOT <mot_edu@outlook.com>
2021-04-16 15:42:41 +0700

    Update

--- resources/views/admin/event_show.blade.php
+++ resources/views/admin/event_show.blade.php
@@ -5,5 +5,47 @@
 @endsection
 
 @section("content")
-    
+    <div>
+        <div>
+            <img class="wp_100" src="{{ asset("photo/".$event->thumbnail.".jpg") }}" alt="">
+        </div>
+        <div>
+            <h3 class="fm-popp color_5">{{ $event->title }}</h3>
+        </div>
+        <div>
+            <span class="color_5">{{ date_format(date_create($event->created_at), "d, M, Y") }}</span>
+        </div>
+        <div class="pb_10 pt_15 fm-popp">
+            <div class="color_5 fw_b">Start date</div>
+            <div>
+                <span>{{ date_format(date_create($event->start), "d, M, Y - h:i:s a") }}</span>
+            </div>
+        </div>
+        <div class="pb_10 fm-popp">
+            <div class="color_5 fw_b">End date</div>
+            <div>
+                <span>{{ date_format(date_create($event->end), "d, M, Y - h:i:s a") }}</span>
+            </div>
+        </div>
+        <div class="pb_10 fm-popp">
+            <div class="color_5 fw_b">Keywords</div>
+            <div>
+                <span>{{ $event->keyword }}</span>
+            </div>
+        </div>
+
+        <div class="pb_10 fm-popp">
+            <div class="color_5 fw_b">Description</div>
+            <div>
+                <span>{{ $event->description }}</span>
+            </div>
+        </div>
+
+        <div class="pb_10 fm-popp">
+            <div class="fw_b pr_5 pl_5 pt_2 pb_2 bcolor_5 color_1">Contents</div>
+            <div>
+                <span>{!! $event->content !!}</span>
+            </div>
+        </div>
+    </div>
 @endsection
