8a85159bbd177eebafa122c48d1f32fd150d3458
EL MOT <mot_edu@outlook.com>
2021-04-18 13:25:42 +0700

    Update

--- resources/views/admin/partner_create.blade.php
+++ resources/views/admin/partner_create.blade.php
@@ -16,11 +16,23 @@
                             <form action="" method="post" enctype="multipart/form-data">
                                 @csrf
                                 @method("post")
-                                <input type="file" name="partner" accept="image/jpeg" hidden>
+                                <input id="partner" type="file" name="partner" accept="image/jpeg" hidden>
                             </form>
-                            <label>
-                                <button class="oln_n bd_n pr_10 pl_10 pt_3 pb_3 bcolor_1" type="button">Upload Logo</button>
+                            <label for="partner">
+                                <span class="oln_n bd_n pr_10 pl_10 pt_3 pb_3 bcolor_5 hbcolor_4 abcolor_4 color_1 fm-popp b_r_3 us_n csr-p">Upload Logo</span>
                             </label>
+                        </div>
+                        <div>
+                            <div class="pb_5">
+                                <div class="fm-popp lh_28 fs_16">Instagram
+                                    @error("instagram")
+                                    <span class="fs_14">&nbsp;:<span class="color_4"> {{ $message }}</span></span>
+                                    @enderror
+                                </div>
+                                <label class="ds_f p-r">
+                                    <input type="text" name="instagram" value="{{ old("instagram") }}" class="input-1 box-s1 fm-popp wp_100 pd-10x15 fs_16 oln_n bd_n b_r_3" placeholder="instagram">
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
