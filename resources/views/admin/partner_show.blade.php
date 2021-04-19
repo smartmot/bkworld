5e10c26321a671ec497953c843b37064044e928f
EL MOT <mot_edu@outlook.com>
2021-04-19 16:43:35 +0700

    Update

--- resources/views/admin/partner_show.blade.php
+++ resources/views/admin/partner_show.blade.php
@@ -5,8 +5,32 @@
 @endsection
 
 @section("content")
-    <div>
-        <div></div>
+    <div class="rowc pt_10">
+        <div class="xl-6 lg-6 md-6 sm-12 fx_12">
+            <div class="w_120 _0auto pb_5">
+                <img class="wp_100" src="{{ asset("photo/".$partner->logo.".jpg") }}" alt="">
+            </div>
+            <div class="color_5 fm-popp">
+                <div>Name &nbsp;:</div>
+                <div class="pl_15">{{ $partner->name }}</div>
+            </div>
+            <div class="color_5 fm-popp">
+                <div class="t_t_c">website &nbsp;:</div>
+                <div class="pl_15">{{ $partner->website }}</div>
+            </div>
+            <div class="color_5 fm-popp">
+                <div class="t_t_c">email &nbsp;:</div>
+                <div class="pl_15">{{ $partner->email }}</div>
+            </div>
+            <div class="color_5 fm-popp">
+                <div class="t_t_c">phone &nbsp;:</div>
+                <div class="pl_15">{{ $partner->phone }}</div>
+            </div>
+            <div class="color_5 fm-popp">
+                <div class="t_t_c">address &nbsp;:</div>
+                <div class="pl_15">{{ $partner->address }}</div>
+            </div>
+        </div>
     </div>
 @endsection
 
