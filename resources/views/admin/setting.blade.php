8a5515b21cb250071059b73d4950253fe2150352
EL MOT <mot_edu@outlook.com>
2021-04-20 13:25:16 +0700

    Update

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -9,7 +9,16 @@
                     <span class="fa fa-edit"></span>
                 </a>
             </div>
-            <div class="pl_15">{{ config("settings.name") }}</div>
+            <div class="bdw_1 bds-s bdcolor_5 pd-5x15">{{ config("settings.name") }}</div>
+        </div>
+        <div class="bd_bs_s bd_bw_1 bdb_color_1 pb_5">
+            <div class="color_5 fs_18">
+                <span class="opc_50">Meta Keyword</span>&nbsp;
+                <a href="#" class="t_d_n color_5 hcolor_4 acolor_4">
+                    <span class="fa fa-edit"></span>
+                </a>
+            </div>
+            <div class="bdw_1 bds-s bdcolor_5 pd-5x15">{{ config("settings.name") }}</div>
         </div>
     </div>
 @endsection
