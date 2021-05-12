7fdee9d83d87a070ee753e2b113ddb2595d59555
EL MOT <mot_edu@outlook.com>
2021-05-12 10:01:59 +0700

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->enum("gender", ["male", "female"]);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('code')->nullable();
             $table->string('token', 400);
             $table->enum('status', ["active", "deleted", "blocked"]);
             $table->enum('role', ["admin", "editor", "moderator"]);
